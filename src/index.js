cb89551a4d91ea94c34a188b510f9105455436d9
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-03-18 19:22:44 +0300

    create basic layout in templte

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import "./styles.css";
 
 console.log("Webpack is working");
+
+//The core idea
+
+//Add tasks
+//delete tasks
+//mark complete
+//edit task
+
+//structure
+//due date
+// priority (low, mid, high)
+//local storage
+//filters:
+// all , today , completed
+
+//for ui
+//we have a primary sidebar.
+//different sections for:
+// all tasks, today, completed, project list, add project
+//main area: Title for exampe today , task list, add task button
